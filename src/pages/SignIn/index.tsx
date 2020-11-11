c1e3016aea9191286602df39e44c3d7cd0ef0f86
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-11-11 13:20:47 -0300

    input| components and css

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {FiLogIn, FiMail,FiLock} from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+
+import {Container, Content, Background} from './styles'
+
+const SignIn: React.FC = ()=> (
+    <Container>
+        <Content>
+        <img src={logoImg} alt="GoBarber"/>
+        <form>
+            <h1>Faça seu logon</h1>
+            <Input icon={FiMail} name="email" placeholder="E-mail"/>
+            <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+            <Button type="submit">Entrar</Button>
+            <a href="forgot">Esqueci minha senha</a>
+        </form>
+            <a href="account">
+                <FiLogIn/>
+                Criar conta
+            </a>
+        
+        </Content>
+        <Background />
+    </Container>
+);
+
+export default SignIn;
